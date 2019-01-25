d027cfae859944efef785c7c365e9b217f278cf1
zhang0peter <zsd780885029@gmail.com>
2019-01-25 08:23:37 +0800

    2019-1-25

--- number.py
+++ number.py
@@ -6,8 +6,8 @@ Created on Fri Sep 14 09:12:16 2018
 """
 
 import matplotlib.pyplot as plt
-date=['2018-5','2018-6','2018-7','2018-8','2018-9','2018-10','2018-11','2018-12']
-number=[27,100,202,75,310,387,265,226]
+date=['2018-5','2018-6','2018-7','2018-8','2018-9','2018-10','2018-11','2018-12','2019-1']
+number=[27,100,202,75,310,387,265,226,186]
 print("total number: ",sum(number))
 plt.title("时间与文章阅读数",fontproperties='SimHei',fontsize=25)
 plt.xlabel("时间",fontproperties='SimHei',fontsize=15)
